5aa188dc7d429d2afbc41fbdd2b8debd088d7731
Conocimientoven <Conocimientoven@users.noreply.github.com>
2018-04-09 21:55:15 -0500

    Código SQL y nueva BD añadida
    Añexo una nueva versión del código utilizado para la BD en SQL; añade las funcionalidades de crear y restaurar la base de datos desde la ruta 'C:\BACKUPS\SDCDB.bak', por lo que hay que mover el archivo de la BD, también anexado en este commit, a la ruta especificada

--- SQL/SDCBD.sql
+++ SQL/SDCBD.sql
@@ -4,16 +4,19 @@ USE SDCDB
 
 create table Usuarios
 (
-Id_Usuario int,
-Nombre_Usuario varchar(20),
+Id_Usuario varchar(20),
+Nombre_Usuario varchar(50),
 Clave_Usuario varchar(30),
-Tipo_Usuario varchar(13)
+Tipo_Usuario varchar(13),
+primary key(Id_Usuario)
 )
 
-INSERT INTO Usuarios VALUES(0,'Admin','Admin','Administrador')
-INSERT INTO Usuarios VALUES(1,'Seller','Seller','Vendedor')
+INSERT INTO Usuarios VALUES('admin','Aurea De La Mora','admin','Administrador')
+INSERT INTO Usuarios VALUES('seller','Kira Yoshikage','seller','Vendedor')
 
 SELECT * FROM Usuarios
+
+drop table usuarios
 
 --OBTAINING THE ROWCOUNT PROCEDURE------------------------
 GO
@@ -31,7 +34,7 @@ execute getRowcount
 GO
 ALTER PROCEDURE selectionForAccess
 AS BEGIN
-SELECT Nombre_Usuario,Clave_Usuario, Tipo_Usuario FROM Usuarios
+SELECT Id_Usuario,Clave_Usuario, Tipo_Usuario FROM Usuarios
 END
 GO
 
@@ -41,18 +44,52 @@ execute selectionForAccess
 
 --NEW USER------------------------
 GO
-CREATE PROCEDURE insertNewUser @ID int, @Nombre varchar(20), @Clave varchar(30), @Tipo varchar(13)
+ALTER PROCEDURE insertNewUser @ID varchar(20), @Nombre varchar(50), @Clave varchar(30), @Tipo varchar(13)
 AS BEGIN
 INSERT INTO Usuarios VALUES(@ID,@Nombre,@Clave,@Tipo)
 END
 GO
 
+
+CREATE TRIGGER checkInsert ON Usuarios
+AFTER INSERT
+AS
+BEGIN
+SET NOCOUNT ON
+IF((SELECT Nombre_Usuario FROM INSERTED)!='' OR ((SELECT LEN(Nombre_Usuario) FROM INSERTED)<10))
+print 'Se requiere escribir un nombre de usuario de al menos 10 caracteres'
+rollback
+end
+
+
+
+INSERT INTO Usuarios VALUES('kira','rrrrrrrrr','kira','Administrador')
+
+
+
+drop trigger checkInsert
+
+create trigger checkInsertOnUnderAge on estudiantes
+after insert
+as
+begin
+set nocount on
+if((select edad from inserted)>18)
+print 'YOU CANNOT ADD THIS USER'
+rollback
+end
+
+
+
+
+
+
 drop procedure inserNewUser
 GO
 
 execute insertNewUser 3, 'Leo', 'Leo', 'Administrador'
-INSERT INTO Usuarios VALUES(1,'Seller','Seller','Vendedor')
-INSERT INTO Usuarios VALUES(0,'Admin','Admin','Administrador')
+INSERT INTO Usuarios VALUES(5,'Seller23','Seller','Vendedor')
+INSERT INTO Usuarios VALUES(10,'Admin','Admin','Administrador')
 
 -----------------------------------------------------------
 
@@ -102,6 +139,30 @@ execute consultUser
 INSERT INTO Usuarios VALUES(1,'Seller','Seller','Vendedor')
 
 -----------------------------------------------------------
+
+--BACKUP DATABASE------------------------
+GO
+CREATE PROCEDURE backupDatabase
+AS BEGIN
+BACKUP DATABASE EDSI TO DISK = 'C:\BACKUPS\EDSI.BAK' WITH DIFFERENTIAL
+END
+GO
+
+execute backupDatabase
+-----------------------------------------------------------
+
+--RESTORE DATABASE------------------------
+GO
+CREATE PROCEDURE restoreDatabase
+AS BEGIN
+RESTORE DATABASE EDSI FROM DISK = 'C:\BACKUPS\EDSI.BAK' WITH REPLACE
+END
+GO
+
+execute restoreDatabase
+-----------------------------------------------------------
+
+
 
 
 
