758aefde9fc0f06e508ad80b70f73df25e35649c
ramonson <ramongarcas@outlook.com>
2018-05-10 12:10:05 -0500

    Comandos
    Comandos para códigos postales, proveedores y productos.

--- SQL/SDCBD.sql
+++ SQL/SDCBD.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE SDCDB
-USE SDCDB
+---------- SDCDB
+create database SDCDB
+use SDCDB
+---------- Fin SDCDB
 
-
-
---COSAS DE LA INTERFAZ ADMINISTRAR USUARIOS*********************************************************************************
+----------Interfaz Administrar Usuarios
 create table Usuarios
 (
 Id_Usuario varchar(20),
@@ -12,263 +12,333 @@ Clave_Usuario varchar(30),
 Tipo_Usuario varchar(13),
 primary key(Id_Usuario)
 )
-
-INSERT INTO Usuarios VALUES('admin','Aurea De La Mora','admin','Administrador')
-INSERT INTO Usuarios VALUES('seller','Kira Yoshikage','seller','Vendedor')
-
-SELECT * FROM Usuarios
-
-drop table usuarios
-
---OBTAINING THE ROWCOUNT PROCEDURE------------------------
-GO
-CREATE PROCEDURE getRowCount
-AS BEGIN
-SELECT COUNT(*) FROM Usuarios WITH (NOLOCK)
-END
-GO
+------------- Obtaining getRowCount 
+go
+create procedure getRowCount
+as begin
+select count(*) from Usuarios with (nolock) end
+go
 
 execute getRowcount
+---------- Fin Obtaining getRowcount
 
------------------------------------------------------------
+---------- Obtaining the Access procedure
+go
+create procedure selectionForAccess2
+as begin
+select Id_Producto,
+Descripción,
+Cantidad_Producto,
+Unidad_Medida 
+from Productos
+en
+go
+---------- Fin Obtaining Access Procedure
 
---OBTAINING THE ACCESS PROCEDURE---------------------------
-GO
-ALTER PROCEDURE selectionForAccess
-AS BEGIN
-SELECT Id_Usuario,Clave_Usuario, Tipo_Usuario FROM Usuarios
-END
-GO
-
-execute selectionForAccess
------------------------------------------------------------
-
-
---NEW USER------------------------
-GO
-ALTER PROCEDURE insertNewUser @ID varchar(20), @Nombre varchar(50), @Clave varchar(30), @Tipo varchar(13)
-AS BEGIN
-INSERT INTO Usuarios VALUES(@ID,@Nombre,@Clave,@Tipo)
-END
-GO
-
-
-CREATE TRIGGER checkInsert ON Usuarios
-AFTER INSERT
-AS
-BEGIN
-SET NOCOUNT ON
-IF((SELECT Nombre_Usuario FROM INSERTED)!='' OR ((SELECT LEN(Nombre_Usuario) FROM INSERTED)<10))
-print 'Se requiere escribir un nombre de usuario de al menos 10 caracteres'
-rollback
-end
-
-
-
-INSERT INTO Usuarios VALUES('kira','rrrrrrrrr','kira','Administrador')
-
-
-
-drop trigger checkInsert
-
-create trigger checkInsertOnUnderAge on estudiantes
+--------- New user procedure
+go
+create procedure insertNewUser 
+@ID varchar(20), 
+@Nombre varchar(50), 
+@Clave varchar(30), 
+@Tipo varchar(13)
+as begin
+insert into Usuarios 
+values(@ID,
+@Nombre,
+@Clave,
+@Tipo) end
+go
+---------- Fin Inser New user procedure
+---------- Trigger para validar los usuarios, nombre
+create trigger checkInsert on Usuarios
 after insert
-as
-begin
+as begin
 set nocount on
-if((select edad from inserted)>18)
-print 'YOU CANNOT ADD THIS USER'
-rollback
-end
-
-
-
-
-
-
-drop procedure inserNewUser
-GO
-
-execute insertNewUser 3, 'Leo', 'Leo', 'Administrador'
-INSERT INTO Usuarios VALUES(5,'Seller23','Seller','Vendedor')
-INSERT INTO Usuarios VALUES(10,'Admin','Admin','Administrador')
-
------------------------------------------------------------
-
---MODIFY USER------------------------
-GO
-ALTER PROCEDURE modifyUser @ID varchar(20), @Nombre varchar(20), @Clave varchar(30), @Tipo varchar(13)
-AS BEGIN
-UPDATE Usuarios SET Nombre_Usuario=@Nombre, Clave_Usuario=@Clave, Tipo_Usuario=@Tipo where Id_Usuario=@ID
-END
-GO
------------------
-drop procedure modifyUser
-GO
-
-execute modifyUser 2, 'Ra', 'Ra', 'Vendedor'
-INSERT INTO Usuarios VALUES(2,'RAasdf','Seller','Vendedor')
-
------------------------------------------------------------
-
-
---DELETE USER------------------------
-GO
-ALTER PROCEDURE deleteUser @ID varchar(20)
-AS BEGIN
-DELETE FROM Usuarios WHERE Id_Usuario=@ID
-END
-GO
-
-drop procedure deleteUser
-GO
-
-execute deleteUser admina
------------------------------------------------------------
-
---CONSULT USER------------------------
-GO
-CREATE PROCEDURE consultUser
-AS BEGIN
-SELECT ID_Usuario, Nombre_Usuario,Clave_Usuario,Tipo_Usuario FROM Usuarios 
-END
-GO
-
-drop procedure consultUser
-GO
-
-execute consultUser
-INSERT INTO Usuarios VALUES(1,'Seller','Seller','Vendedor')
-
------------------------------------------------------------
-
-
-DELETE FROM Usuarios where Id_Usuario=0
-
-
-
-
---TERMINAN LAS COSAS DE LA INTERFAZ ADMINISTRAR USUARIOS********************************************************************************
-
-
-
-
-
-
-
-
-
-
---COSAS DE LA INTERFAZ ADMINISTRAR BASE DE BD*********************************************************************************
-
-
-
---BACKUP DATABASE------------------------
-GO
-ALTER PROCEDURE backupDatabase
-AS BEGIN
-BACKUP DATABASE SDCDB TO DISK = 'C:\BACKUPS\SDCDB.BAK' WITH DIFFERENTIAL
-END
-GO
-
-execute backupDatabase
------------------------------------------------------------
-
---RESTORE DATABASE------------------------
-GO
-ALTER PROCEDURE restoreDatabase
-AS BEGIN
-RESTORE DATABASE SDCDB FROM DISK = 'C:\BACKUPS\SDCDB.BAK' WITH REPLACE
-END
-GO
-
-execute restoreDatabase
------------------------------------------------------------
-
-
-
-
-
-
---EJEMPLOS XDXDX
-
-
-
--- Esto hace un backup de nuestra base de datos
-BACKUP database SDCDB
-to disk = 'C:\BACKUPS\SDCDB.bak'
-
--- drop database sistec
--- esto elimina la base de datos y si no hay backup valio :'c
-
-
--- Esto restaura la base de datos
-restore database SDCDB
-FROM disk = 'C:\BACKUPS\SDCDB.bak'
-WITH REPLACE
-
-
-
-
-
---TERMINAN LAS COSAS DE LA INTERFAZ ADMINISTRAR BD********************************************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---COSAS DE LA INTERFAZ ADMINISTRAR PRODUCTOS***********************************************************************
-
-
-create table Productos
-(
-Id_Producto varchar(20),
-Descrip_Producto varchar(100),
-Cantidad_Producto varchar(10),
-Unidad_Medida_Producto varchar(6),
-primary key(Id_Producto)
-)
-drop table Productos
-
-
-
-
-INSERT INTO Productos VALUES('Nutella','Es uno de los ingredientes más solicitados en las crepas. Es vital cuidarlo.','5','Kilos')
-
-SELECT * FROM Productos
-
-
-
-
-
-
---INSERTAR PRODUCTO
-GO
-CREATE PROCEDURE insertNewProduct @ID varchar(20), @Descrip varchar(100), @Cantidad varchar(10), @Unidad_Medida varchar(6)
-AS BEGIN
-INSERT INTO Usuarios VALUES(@ID,@Descrip,@Cantidad,@Unidad_Medida)
-END
-GO
-
-DROP PROCEDURE insertNewProducto
-
-EXECUTE insertNewProduct('Crema de cacahuate', 'Sirve para la crepa 13', '2','Litros')
---TERMINA INSERTAR PRODUCTO
-
-
-
-
-
+if((select Nombre_Usuario from INSERTED)!='' 
+OR ((select len(Nombre_Usuario) from INSERTED) < 10))
+print 'Se requiere escribir un nombre de usuario de al menos 10 caracteres'
+rollback end
+---------- Fin trigger para validar usuarios, nombre
+
+---------- Trigger para validar estudiantes, edad
+--create trigger checkInsertOnUnderAge on estudiantes
+--after insert
+--as
+--begin
+--set nocount on
+--if((select edad from inserted)>18)
+--print 'YOU CANNOT ADD THIS USER'
+--rollback
+--end---------- Fin trigger para validar estudiantes, edad
+
+
+
+---------- Procedimiento para modificar usuarios
+go
+create procedure modifyUser 
+@ID varchar(20), @Nombre varchar(20),
+@Clave varchar(30), 
+@Tipo varchar(13)
+as begin
+update Usuarios set 
+Nombre_Usuario=@Nombre, 
+Clave_Usuario=@Clave, 
+Tipo_Usuario=@Tipo 
+where Id_Usuario=@ID end
+go
+---------- Fin procedimiento para modificar usuarios
+
+---------- Procedimiento para eliminar u usuario
+go
+create procedure deleteUser 
+@ID varchar(20)
+as begin
+delete 
+from Usuarios where Id_Usuario=@ID end
+go
+---------- Fin procedimiento para eliminar un usuario
+
+---------- Procedimiento para consultar un usuario
+go
+create procedure consultUser
+as begin
+select ID_Usuario, 
+Nombre_Usuario,
+Clave_Usuario,
+Tipo_Usuario
+from Usuarios end
+go
+
+---------- Fin procedimiento para conultar por un usuario
+---------- Fin Interfaz administrar usuarios
+
+---------- Administrar Proveedores interfaz
+--create table Proveedores(
+--Id_Proveedor varchar(8),
+--Nombre_Proveedor varchar(50),
+--Teléfono_Proveedor varchar(10),
+--Código_Postal varchar(5),
+--Ciudad_Proveedor varchar(40),
+--Colonia_Proveeddor varchar (40),
+--Calle_Proveedor varchar (40),
+--Número_Calle varchar (4),
+--primary key(Id_Proveedor
+--)
+---------- Procedimiento para crear un nuevo provedor ----------
+
+
+go
+create procedure addNewProvider
+@Id_Proveedor varchar(8),
+@Nombre_Proveedor varchar (50),
+@Teléfono_Proveedor varchar (10),
+@Código_Postal varchar (5),
+@Estado_Proveedor varchar (5000),
+@Municipio varchar (5000),
+@Colonia varchar (5000),
+@Calle varchar (200),
+@Número_Calle varchar (5)
+as begin
+insert into Proveedores 
+values(
+@Id_Proveedor,
+@Nombre_Proveedor,
+@Teléfono_Proveedor,
+@Código_Postal,
+@Estado_Proveedor,
+@Municipio,
+@Colonia,
+@Calle,
+@Número_Calle
+) end
+go
+---------- Fin procedimiento para crear un nuevo proveedor ----------
+---------- Procedimiento para modificar un proveedor
+go
+create procedure newModifyProvidersProcedure
+@Id_Proveedor varchar(8),
+@Nombre_Proveedor varchar (50),
+@Teléfono_Proveedor varchar (10),
+@Código_Postal varchar (5),
+@Estado_Proveedor varchar (5000),
+@Municipio varchar (5000),
+@Colonia varchar (5000),
+@Calle varchar (200),
+@Número_Calle varchar (5)
+as begin
+update Proveedores set 
+Id_Proveedor = @Id_Proveedor,
+Nombre_Proveedor = @Nombre_Proveedor,
+Teléfono_Proveedor = @Teléfono_Proveedor,
+Código_Postal = @Código_Postal,
+Estado_Proveedor = @Estado_Proveedor,
+Municipio = @Municipio,
+Colonia = @Colonia,
+Calle = @Calle,
+Número_Calle = @Número_Calle
+where Id_Proveedor = @Id_Proveedor end
+go
+---------- Fin procedimiento modifi ar proveedor
+---------- Procedimiento para eliminar un proveedor
+go
+create procedure providersEraser 
+@Id_Proveedor varchar(8)
+as begin
+delete 
+from Proveedores where Id_Proveedor = @Id_Proveedor end
+go
+---------- Fin procedimiento para eliminar un proveedor
+---------- Procedimiento para consultar un proveedor
+select * from Proveedores
+go
+create procedure consultProvider2
+as begin
+select 
+Id_Proveedor, 
+Nombre_Proveedor,
+Teléfono_Proveedor,
+Código_Postal
+Estado_Proveedor,
+Municipio,
+Colonia,
+Calle,
+Número_Calle
+from Proveedores end
+go
+execute consultProvider2
+insert into Proveedores values ('15290881','Ramón García de Alba Castrejón','3411252145','49020','Jalisco','Zapotlán el grande','Infonavit','Moctezuma','644')
+---------- Procedimiento proveedores ----------
+go
+create procedure getRowCountProviders2
+as begin
+select count(*) from Proveedores with (nolock) end
+go
+---------- Fin procedimiento proveedores ----------
+--------- Fin procedimiento para consultar un proveedor
+--------- Fin interfaz administrar proveedores
+
+--------- Administrar base de datos
+--------- Procedimiento para respaldar base de datos 
+go
+create procedure backupDatabase
+as begin
+backup database SDCDB to disk = 'C:\respaldosSDCDB\SDCDB.BAK'
+with differential end
+go
+---------- Fi procedimiento para respaldar la base de datos
+---------- Procedimiento pra restaurar la babse de datos
+go
+create procedure restoreDatabase2
+as begin
+restore database SDCDB from disk = 'C:\\ftpramon\rdos.bak' with replace end
+go
+--------- Fin procedimiento para restaurar la base de datos
+---------- Fin interfaz Administrar base de datos
+---------- Ejemplos de la clase de juanito ----------
+
+----- Aquín se respalda la base de datos -----
+--backup database SDCDB to disk = 'C:\BACKUPS\SDCDB.bak'
+----- Fin respaldar base de datos -----
+
+----- Esto restaura la base de datos -----
+--restore database SDCDB from disk = 'C:\BACKUPS\SDCDB.bak' with replace
+----- Fin restaurar la base de datos -----
 
 --TERMINAN LAS COSAS DE LA INTERFAZ ADMINISTRAR PRODUCTOS*****************************************************
+
+------Administrar Productos
+drop table Productos
+create table Productos(
+Id_Producto varchar(30),
+Descripción varchar(300),
+Cantidad_Producto varchar(12),
+Unidad_Medida varchar (20),
+primary key (Id_Producto)
+)
+
+
+----Agregar un producto procedure
+go
+create procedure newProducts3
+@Id varchar(30), 
+@Descripción varchar(300), 
+@Cantidad varchar(12), 
+@Unidad_Medida varchar(20)
+as begin
+insert into Productos values(@Id,@Descripción,@Cantidad,@Unidad_Medida) end
+go
+
+execute newProduct 'bote de pasta','pa, bailar','1','Bote'
+------ Fin agregar un producto
+------ Modificar un prodcuto 
+--drop procedure modifyProduct
+go
+create procedure modifyProducts3
+@Id varchar(30), 
+@Descripción varchar(300), 
+@Cantidad varchar(100),
+@Unidad_Medida varchar (13)
+as begin
+UPDATE Productos set 
+Descripción = @Descripción, 
+Cantidad_Producto = @Cantidad, 
+Unidad_Medida = @Unidad_Medida
+where Id_Producto = @Id end
+go
+------ Fin modificar un producto
+------ Eliminar un producto 
+--drop procedure deleteProduct
+select * from Productos
+go
+create procedure deleteProduct3 @Id varchar(20)
+as begin
+delete from Productos where Id_Producto = @Id end
+go
+------ Fin eliminar un producto
+------ Consultar un producto
+--drop procedure consultProducts
+go
+create procedure consultProducts3
+as begin
+select Id_Producto, 
+Descripción,
+Cantidad_Producto,
+Unidad_Medida
+from Productos end
+go
+
+select * from Productos
+execute consultProducts
+
+------ Fin consultar un producto
+------ Contador de renglones 
+go
+create procedure getProductosCountrows3
+as begin
+select count(*) from Productos with (nolock) end
+go
+
+------ Fin contador de renglones
+------ Fin administrar productos
+---------- Procedimiento CódigoPostal
+go
+create procedure consultPostalCodes
+as begin
+select 
+CodigoPostal,
+Colonia,
+Municipio,
+Estado
+from CodigosPostales end
+go
+execute consultPostalCodes
+select * from CodigosPostales
+--------- Fin procedimiento códigoPostal
+---------- getCounterRow Postal codes --------
+go
+create procedure getCount
+as begin
+select count(*) from CodigosPostales with (nolock) end
+go
+---------- Fin getCounterRow Postal codes --------
